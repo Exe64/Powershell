2c2acf1ce77077d30f87a526dc358191cd5a7951
Exe64 <julien.dumur@outlook.com>
2020-01-06 12:01:41 +0100

    Update export-ad-users.ps1

--- export-ad-users.ps1
+++ export-ad-users.ps1
@@ -1,7 +1,7 @@
 #Export users list from AD OU in a .csv file
 
 #Variables 
-$ou = "THE OU YOU WANT TO EXPORT"
+$ou = "OU=WHATE,OU=YOU,OU=WANT,DC=TO,DC=EXPORT"
 $path = c:\PATH\TO\FILE.csv
 
 Get-ADUser -filter { enabled -eq $True } -SearchBase $ou –properties sn,Givenname,Samaccountname,mail,telephoneNumber,mobile,department | select-object sn,Givenname,Samaccountname,mail,telephoneNumber,mobile,department | export-csv –path $path –delimiter ";"
